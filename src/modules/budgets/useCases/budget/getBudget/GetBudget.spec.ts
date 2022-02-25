fff1f66dcdd7c72c4cae03e56c8b13133f94d4ba
Maica A <almaicaa@gmail.com>
2022-02-25 23:28:06 +0800

    Implement GetBudget use case

---
+++ src/modules/budgets/useCases/budget/getBudget/GetBudget.spec.ts
@@ -0,0 +1,37 @@
+import { GetBudget } from './GetBudget';
+
+import { budgetRepository } from 'modules/budgets/repos';
+import { Guid } from 'shared/domain';
+
+import faker from '@faker-js/faker';
+
+jest.mock('modules/budgets/repos/implementations/budgetRepository');
+
+describe('GetBudget', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should return the correct budget given the id', async () => {
+		const randomBudgetId = new Guid(faker.datatype.uuid());
+		const getBudget = new GetBudget(budgetRepository);
+
+		const budgetResult = await getBudget.execute({ budgetId: randomBudgetId });
+
+		expect(budgetResult.isSuccess).toBeTruthy();
+		expect(budgetResult.value?.id).toEqual(randomBudgetId);
+	});
+
+	it('should return an error if budget is not found', async () => {
+		jest.spyOn(budgetRepository, 'getBudgetById').mockImplementation(() => {
+			throw new Error('Budget not found');
+		});
+
+		const randomBudgetId = new Guid(faker.datatype.uuid());
+		const getBudget = new GetBudget(budgetRepository);
+
+		const budgetResult = await getBudget.execute({ budgetId: randomBudgetId });
+
+		expect(budgetResult.isFailure).toBeTruthy();
+	});
+});
---
+++ src/modules/budgets/useCases/budget/getBudget/GetBudget.ts
@@ -0,0 +1,31 @@
+import Budget from 'modules/budgets/domain/entities/budget';
+import { IBudgetRepository } from 'modules/budgets/repos/interfaces/budgetRepository';
+
+import { Guid, UseCase } from 'shared/domain';
+import { Result } from 'shared/core/Result';
+
+interface IGetBudgetDTO {
+	budgetId: Guid;
+}
+
+type GetBudgetResponse = Result<Budget | undefined>;
+
+export class GetBudget implements UseCase<IGetBudgetDTO, GetBudgetResponse> {
+	private budgetRepository: IBudgetRepository;
+
+	constructor(budgetRepository: IBudgetRepository) {
+		this.budgetRepository = budgetRepository;
+	}
+
+	async execute(request: IGetBudgetDTO): Promise<GetBudgetResponse> {
+		try {
+			const budget = await this.budgetRepository.getBudgetById(
+				request.budgetId
+			);
+
+			return Result.ok(budget);
+		} catch (err) {
+			return Result.fail(err as Error);
+		}
+	}
+}
